705b7f9a19c400c5eea29e2ba2e37b4b9daba3ec
Arthur Scarpati <arthur_159@hotmail.com>
2021-11-11 18:16:51 -0300

    Css styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,7 +1,9 @@
+import 'assets/styles/custom.scss';
 import "./App.css";
+import NavBar from "components/NavBar";
 
 function App() {
-  return <h1>Olá</h1>;
+  return <NavBar />
 }
 
 export default App;
---
+++ frontweb/src/components/NavBar/index.tsx
@@ -0,0 +1,41 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary nav-container" >
+      <div className="container-fluid title-container">
+        <a href="link" >
+          <h4>Carros Top</h4>
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#dscarros-navbar"
+          aria-controls="dscarros-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse main-div" id="dscarros-navbar">
+          <ul className="navbar-nav offset-md-2 main-text">
+            <li>
+              <a href="link">Home</a>
+            </li>
+            <li>
+              <a href="link">Catálogo</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    
+  );
+};
+
+export default NavBar;
